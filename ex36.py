be07813c4683db2f3d04d4277ea6697638f3e22b
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-10-18 14:13:07 -0500

    add all pseudocode for outline of game

--- ex36.py
+++ ex36.py
@@ -1,17 +1,79 @@
 # Text-based Strategy Game: Your First Meetup in A Foreign Country
 # Exercise 36 in Learn Python the Hard Way
 
+# LEAVE FUNCTION
+# go home and end game
+
+# LIST OF BEERS
+# Victoria, Indio, a local IPA, Corona Light
+
+# LIST OF SNACKS
+# Cacahuates japoneses, chicharon, chapulines
+
 
 # FOYER
+# Print intro language
 
+# Decision point: check in dude speaking Spanish v quickly c slang and waiving a roll of raffle tickets around. No compredes.
+# 1. Turn around, leave, go home --> leave function
+# 2. Decide to stay and try your best to learn something and talk to strangers.
+# Tell him you are learning Spanish y por fa' despacio. He apologizes, repeats himself more slowly. He's telling you 
+# about your free beer ticket and a snack. He lists the beers available, you must select your choice so he can write it down.
 
+# Print list of beers
+# Select and store beer choice.
+
+# Print list of snacks.
+# Select and store snack choice.
+
+# He gives you two tickets and tells you to continue upstairs. --> Bar
 
 
 
 # BAR
 
+# Describe the terrace that has been converted to a make-shift bar, where people are standing around in small groups.
 
+# Everyone seems to know each other and is speaking Spanish. Ug. Do you:
+# 1. Turn around, leave, go home --> leave function
+# 2. Decide to stay and try your best to learn something and talk to strangers.
+
+# Give yourself something to do, walk up to the bar, and hold out your drink ticket.
+# Bartender takes your drink ticket, gives the list of options
+# Select your choice.
+
+# The talk doesn't start for 30 minutes, what do you do?
+# 1. Slam your beer and order more
+    # How many more beers do you drink?
+    # one more: drink it, then --> presentation room
+    # 2 - 3 more: get lost trying to find bathroom, finally do and then discover there's no toilet paper. --> presentation room
+    # 4+ more: Wander into the offices while trying to find the bathroom and end up locked in their bathroom. Miss 
+    # presentations --> end game
+# 2. Drink your beer (maybe make some friends)
+    # A woman in a group standing next to you at the bar says hi. She's wearing blinking ßrainbow LEDs in her hair.
+    # 1. You tell her they're cool: she says thanks and starts telling you how she programmed them. You talk w the group.
+        # A guy comes up with a bag of snacks, he lists them and asks which you would like.
+        # leave and --> leave function
+        # --> presentation room (skip where to sit)
+    # 2. You take a swig of your beer, then --> presentation room
+# 3. Take your beer into the presentation room (dodge creeper)
+    # Good job! A creeper had been eyeing you and was just about to walk over and start hitting on you --> presentation room
 
 
 
 # PRESENTATION ROOM
+# Where to sit
+# 1. Front row: in presenter's seat, have to move, loop back to where to sit
+# 2. Few rows back: Good job! You can see and hear the presentations --> After presentations
+# 3. Back of the room: can't hear, barely see anything. --> After presentations
+
+# After presentations
+# The presenations have finished, as you're getting up to leave you see s/o at the end of your row putting away a laptop with
+# a bunch of R Studio stickers on it. You have been learning about large data sets and creating data visualizations, what do you do:
+# 1. Leave --> leave function
+# 2. Say in broken Spanish "What do you use R Studio for?" He works for a company using data for social justice projects. He
+# invites you to a lunchtime talk later that week at his work. A co-worker will be talking about their latest data visualization project. 
+# What do you do
+# 1. Accept invite, attend talk, and ultimately land a great internship where you learn tons and work on great projects. This 
+# work directly leads to your first job and a successful and exciting career in technology.
+# 2. Make some non-commital reply and quickly --> Leave function
